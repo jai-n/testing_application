f98e6b551b522fd850cb731b9aa468ce7060e2de
madhusudhan <madhusudan.m@cisinlabs.com>
2013-12-31 12:40:25 +0530

    copy copter example initial commit

---
+++ Copy_copter_example/config/initializers/copycopter.rb
@@ -0,0 +1,9 @@
+# CopycopterClient.configure do |config|
+#   config.api_key = '79a921b3dcc0b902c827cb835dc087518a4deaaba7517ae8'
+#   config.host = 'aqueous-cove-4688.herokuapp.com'
+# end
+
+CopycopterClient.configure do |config|
+  config.api_key = 'bf091d7a5f795ffa50e2f7ed11b740a8ea8c63bd228b7962'
+  config.host = 'aqueous-cove-4688.herokuapp.com'
+end
---
+++ Copy_copter_example/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'e17474fe410535611d6262dcb24c9dce0c8f5d132bf6cb2261f355d73dc5f011d1a2ebccb12be7cbcfe8b6a5385f69b00404da4b9d003e1aa175fa4ea1f8899e'
