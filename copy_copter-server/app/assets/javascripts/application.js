1e1ab561110eb172d677e21c0527589cc6d3b717
madhusudhan <madhusudan.m@cisinlabs.com>
2013-12-31 12:51:12 +0530

    add copy copter server

---
+++ copy_copter-server/app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.localscroll
+//= require jquery.scrollTo
+//= require jquery.wysiwyg
+//= require json2
+//= require incremental_search
+//= require locale_dropdown
+//= require prefilled_input
+//= require_tree .
---
+++ copy_copter-server/public/assets/application-c878ba34d65d02eae91247389119491d.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.localscroll
+//= require jquery.scrollTo
+//= require jquery.wysiwyg
+//= require json2
+//= require_tree .
+;
